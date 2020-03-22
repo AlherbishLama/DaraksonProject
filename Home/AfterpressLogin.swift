8fe242386b8aa275d44dc9c61acd84f61a2fba88
reema <reeroo02@gmail.com>
2020-03-22 20:17:41 +0300

    update land page after log-in

--- Home/AfterpressLogin.swift
+++ Home/AfterpressLogin.swift
@@ -72,7 +72,7 @@ class AfterpressLogin: UIViewController ,UITableViewDataSource, UITableViewDeleg
                          self.navigationController?.pushViewController(storyboard, animated: true)
         } else if item == "Help" {
                               //    let storyboard = self.storyboard?.instantiateViewController(identifier: "DeveloperGame") as! DeveloperGames
-                                        openURL(url: "https://ilv0hoq7zrpnr8zqkghitw-on.drv.tw/Sprint3/www.landingpage/index")
+                                        openURL(url: "https://ilv0hoq7zrpnr8zqkghitw-on.drv.tw/Sprint3/www.landingpage/index3.html")
                                         //   self.navigationController?.pushViewController(storyboard, animated: true)
                              }
 
