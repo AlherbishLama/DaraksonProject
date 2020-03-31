1469613c4cee890c956914c5d073c448ac4cf111
reema <reeroo02@gmail.com>
2020-03-31 22:28:09 +0300

    SecondPartRegisteration / Refactor Status : Completed

--- Home/SignUp/SecondPartRegisteration.swift
+++ Home/SignUp/SecondPartRegisteration.swift
@@ -1,37 +1,29 @@
-//
 //  SecondPartRegisteration.swift
 //  Home
-//
-//  Created by Lama Alherbish on 2/3/20.
-//  Copyright © 2020 Lama Alherbish. All rights reserved.
-//
+//  Copyright © 2020 Darakson
+//------------------------Refactor Status : Completed.---------------------------------------------
+//Importing essintial dependencies and freamworks here
+import Foundation // provides the NSObject root class
+import UIKit // for UI object
+import UserNotifications // will have to request permission from the user to
 
-import Foundation
-import UIKit
-import UserNotifications
-
+//-----------------------------start of the class---------------------------------------------------------
 class SecondPartRegisteration : UIViewController , UITextFieldDelegate {
-    
+    // 6 Fields for User PAssword
     @IBOutlet weak var passField1: UITextField!
-    
     @IBOutlet weak var passField2: UITextField!
-    
     @IBOutlet weak var passField3: UITextField!
-    
     @IBOutlet weak var passField4: UITextField!
-    
     @IBOutlet weak var passField5: UITextField!
-    
     @IBOutlet weak var passField6: UITextField!
     
     @IBOutlet weak var ErrorLabel: UILabel!
-
     @IBOutlet weak var NextButtonToTheRegisterPart3: UIButton!
-    
+// to use in other storyboards.
     struct globel2{
         static var completePassword = String()
     }
-    
+//------------------------------viewDidLoad()-----------------------------------------------
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,61 +55,53 @@ class SecondPartRegisteration : UIViewController , UITextFieldDelegate {
              UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
          }
     }
-    
+//---------------------------to restrict the user to enter only numbers--------------------------------
+
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        let allowCharacters = CharacterSet.decimalDigits // to restrict the user to enter only numbers
+        let allowCharacters = CharacterSet.decimalDigits
         let characterSet = CharacterSet(charactersIn: string)
         var startString = ""
-        
         if textField.text != nil {
             startString += textField.text!
         }
-
         startString += string
-
         var limitNumber = Int(startString) ?? 0
-
         if limitNumber > 9 {
             return allowCharacters.isSuperset(of: characterSet) && false
-
         } else {
             return allowCharacters.isSuperset(of: characterSet) && true
-
+            }//end else
         }
-      
-        }
-    
+//--------------------move the cursur after the user enters one digit--------------------------------------
     
     @objc func textFieldDidChange(_ textField: UITextField){
            if textField == passField1 {
                       if (textField.text?.count)! == 1 {
-                          passField2?.becomeFirstResponder()
+                      passField2?.becomeFirstResponder()
                       }
-                  }
-                  else if textField == passField2 {
+                  } else if textField == passField2 {
                       if (textField.text?.count)! == 1 {
-                          passField3.becomeFirstResponder()
+                      passField3.becomeFirstResponder()
                       }
                   } else if textField == passField3 {
-                  if (textField.text?.count)! == 1 {
+                     if (textField.text?.count)! == 1 {
                       passField4.becomeFirstResponder()
-               }}else if textField == passField4 {
-                  if (textField.text?.count)! == 1 {
+                      }
+                  }else if textField == passField4 {
+                     if (textField.text?.count)! == 1 {
                       passField5.becomeFirstResponder()
-               }} else if textField == passField5 {
+                      }
+                 } else if textField == passField5 {
                   if (textField.text?.count)! == 1 {
                       passField6.becomeFirstResponder()//resignFirstResponder()
-               }}else if textField == passField6 {
-               if (textField.text?.count)! == 1 {
-                   passField6.resignFirstResponder()
-                   
-               }}
-           
-           
+                      }
+                }else if textField == passField6 {
+                  if (textField.text?.count)! == 1 {
+                      passField6.resignFirstResponder()
+                        }
+                }
           }
-
-    
+//---------------------------------------KeyBoardHiding---------------------------------------------------
     
     //Presses return key to hide the key board
        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -128,53 +112,38 @@ class SecondPartRegisteration : UIViewController , UITextFieldDelegate {
        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
            self.view.endEditing(true)
        }
-    
+//--------------------------validate if all password fields are filled-----------------------------------
     
     func validateTextField () -> String? {
-    
     if passField1.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||  passField2.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||  passField3.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||  passField4.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || passField5.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || passField6.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""{
            return "please fill the field."
        } //it will remove all spaces and lines
-       
        else{
-           let p1 = (passField1.text!.trimmingCharacters(in: .whitespacesAndNewlines))
-               
+               let p1 = (passField1.text!.trimmingCharacters(in: .whitespacesAndNewlines))
                let p2 = passField2.text!.trimmingCharacters(in: .whitespacesAndNewlines)
                let p3 = passField3.text!.trimmingCharacters(in: .whitespacesAndNewlines)
                let p4 = passField4.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-           
-           let p5 = passField5.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-           
-           let p6 = passField6.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+               let p5 = passField5.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+               let p6 = passField6.text!.trimmingCharacters(in: .whitespacesAndNewlines)
                    
            globel2.completePassword = p1 + p2 + p3 + p4 + p5 + p6
-           
        return nil
-       }}
-    
-    
-    
+           }
+    }
+//----------------------------------------only move next when no error----------------------------------------------
+     
     @IBAction func Next(_ sender: Any) {
-          //validate
                 let error = validateTextField()
-                
                 if error != nil { //that's means that there's an error
                     ErrorLabel.text = error!
                     ErrorLabel.alpha = 1
                     NextButtonToTheRegisterPart3.isEnabled = true
                 } else {
-                    
-                    
                     let storyboard = self.storyboard?.instantiateViewController(identifier: statics.ThirdPartRegestration) as! thirdPartRegisteration
-                                                
-                             self.navigationController?.pushViewController(storyboard, animated: true)
-                }
-              
-            
-            
-        
-    }
-    
+                                     self.navigationController?.pushViewController(storyboard, animated: true)
+                        }
+        }
+ //------------------------------------end of class---------------------------------------------
     
     
 }
--- Home/SignUp/SignUpController.swift
+++ Home/SignUp/SignUpController.swift
@@ -1,26 +1,23 @@
 //  SignUpController.swift
 //  Home
 //  Copyright © 2020 Darakson
-//----------------------------------------------------------------------------------------------------
-//Refactor Status : Completed.
-//----------------------------------------------------------------------------------------------------
-// Importing essintial dependencies here
+//------------------------Refactor Status : Completed.---------------------------------------------
+// Importing essintial dependencies and freamworks here
 import UIKit // for UI object
 import FirebaseAuth // Pod for User Authintication from Firebase.
 import Firebase // for linking the project with firebase.
 import UserNotifications // will have to request permission from the user to recive Notification.
-//----------------------------------------------------------------------------------------------------
-// start of the class 
+//-------------------------------start of the class---------------------------------------------
 class SignUpController: UIViewController , UITextFieldDelegate{
     @IBOutlet weak var CarNameTextField: UITextField! //recive car name from the user.
     @IBOutlet weak var NextButtonToTheRegisterPart2: UIButton!
     @IBOutlet weak var ErrorLabel: UILabel!
     
-    // to use the name in other storyboards.
+    // to use in other storyboards.
     struct global1{
         static var carName = String()
         }
-//----------------------------------------------------------------------------------------------------
+//--------------------------------viewDidLoad()-----------------------------------------------
     
     override func viewDidLoad() { //start of func
         super.viewDidLoad()
@@ -36,26 +33,26 @@ class SignUpController: UIViewController , UITextFieldDelegate{
              UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
         }
     }//end of viewDidLoad()
-//----------------------------------------------------------------------------------------------------
-    // to restrict the user to enter only alpha
+//------------------------ to restrict the user to enter only alpha----------------------------------
+   
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
              let allowCharacters = CharacterSet.letters
              let characterSet = CharacterSet(charactersIn: string)
                  return allowCharacters.isSuperset(of: characterSet)
              } // end of textField()
-//----------------------------------------------------------------------------------------------------
-    //Presses return key to hide the key board
+//-----------------------------Presses return key to hide the key board---------------------------------
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
-//----------------------------------------------------------------------------------------------------
-    // to hide the keyboard when the user preeses outside
+//---------------------------------to hide the keyboard when the user preeses outside--------------------
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-//----------------------------------------------------------------------------------------------------
-    //it will remove all spaces and lines
+//-------------------------it will remove all spaces and lines then validate-------------------------------------------
+    
     func validateTextField () -> String? {
         if CarNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
             return "please fill the field."
@@ -64,8 +61,8 @@ class SignUpController: UIViewController , UITextFieldDelegate{
          global1.carName = CarNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
         return nil
         }}
-//----------------------------------------------------------------------------------------------------
-    //validate
+//----------------------------validate------------------------------------------------------------------
+
     @IBAction func Next(_ sender: Any) {
         let error = validateTextField()
         if error != nil { //that's means that there's an error
@@ -78,5 +75,5 @@ class SignUpController: UIViewController , UITextFieldDelegate{
         } //else
     }// end of Next()
     
-    
-} //end of class.
+  //------------------------------------end of class---------------------------------------------
+}
